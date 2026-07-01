f1d2e334b75cec3a284bc821a81e33634cdfd9d4
Mainuddin Khaled <mainuddinshahar1393@gmail.com>
2026-07-01 21:05:14 +0600

    Final Commit

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -2,22 +2,21 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float speed;
     Vector3 startPosition;
     Vector3 endPosition;
-    [SerializeField] Vector3 movementVector;
-    [SerializeField] float speed = 0f;
-    float movementFactor = 0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float movementFactor;
+
     void Start()
     {
         startPosition = transform.position;
         endPosition = startPosition + movementVector;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        movementFactor = Mathf.PingPong(Time.deltaTime * speed, 1f);
-        transform.position = Vector3.Lerp(startPosition, endPosition, movementFactor);
+        movementFactor = Mathf.PingPong(Time.time * speed, 1f);
+        transform.position = Vector3.Lerp(startPosition,endPosition, movementFactor);
     }
 }
